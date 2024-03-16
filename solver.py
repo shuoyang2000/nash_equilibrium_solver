70681aacbb51c025cf76de5282286e55a68d477c
shuoyang2000 <yangs1@seas.upenn.edu>
2024-03-16 17:37:57 -0400

    rename some variables

--- solver.py
+++ solver.py
@@ -4,18 +4,18 @@ import time
 
 class nash_equlibrium_solver:
 
-    def __init__(self, game_step:int, action_choice_player1:int, action_choice_player2:int, payoff_matrix:np.array):
+    def __init__(self, game_step:int, p1_action_num:int, p2_action_num:int, payoff_matrix:np.array):
         
         """
         game_step: step number of the game; normal game step is 1, extensive game step is more than 1
-        action_choice_player1: player 1's action number
-        action_choice_player2: player 2's action number
+        p1_action_num: player 1's action number
+        p2_action_num: player 2's action number
         payoff_matrix: the generated payoff matrix of the game
         """
 
         self.game_step = game_step
-        self.action_choice_player1 = action_choice_player1
-        self.action_choice_player2 = action_choice_player2
+        self.action_choice_player1 = p1_action_num
+        self.action_choice_player2 = p2_action_num
         self.action_total_num_player1 = self.action_choice_player1 ** game_step
         self.action_total_num_player2 = self.action_choice_player2 ** game_step
         self.payoff_matrix = payoff_matrix
@@ -23,7 +23,7 @@ class nash_equlibrium_solver:
         assert self.payoff_matrix.shape[0] == self.action_total_num_player1, "Payoff matrix row number does not match Player 1 pure strategy number"
         assert self.payoff_matrix.shape[1] == self.action_total_num_player2, "Payoff matrix column number does not match Player 2 pure strategy number"
     
-    def solve_linear_program_player1(self):
+    def solve_linear_program_player1(self, verbose: bool=False):
 
         start_time = time.time()
 
@@ -60,21 +60,23 @@ class nash_equlibrium_solver:
 
         LP_model.optimize()
 
-        ## verbose output
-        print("Player 1 strategy solving time: ", time.time() - start_time)
+        ## return solved outcomes
         if LP_model.Status == GRB.Status.OPTIMAL:
             player1_NE = LP_model.getAttr('x', player1_actions)
             filtered_player1_NE = {k: v for k,v in player1_NE.items() if v > 0.}
             NE_utility = LP_model.ObjVal
-            print("Nash Equlibrium Profile Utility for Player 1: ", NE_utility)
-            print("Nash Equlibrium Player 1 Strategy: ", filtered_player1_NE)
+            if verbose:
+                print("Player 1 strategy solving time: ", time.time() - start_time)
+                print("Nash Equlibrium Profile Utility for Player 1: ", NE_utility)
+                print("Nash Equlibrium Player 1 Strategy: ", filtered_player1_NE)
+                print("------------------------------------------")
             return (filtered_player1_NE, NE_utility)
         else:
             print(LP_model.Status, GRB.Status.OPTIMAL)
             print("The linear programming for player 1 is infeasible, please double check.")
             return None
     
-    def solve_linear_program_player2(self):
+    def solve_linear_program_player2(self, verbose: bool=False):
 
         start_time = time.time()
 
@@ -111,14 +113,16 @@ class nash_equlibrium_solver:
 
         LP_model.optimize()
 
-        ## verbose output
-        print("Player 2 strategy solving time: ", time.time() - start_time)
+        ## return solved outcomes
         if LP_model.Status == GRB.Status.OPTIMAL:
             player2_NE = LP_model.getAttr('x', player2_actions)
             filtered_player2_NE = {k: v for k,v in player2_NE.items() if v > 0.}
             NE_utility = LP_model.ObjVal
-            print("Nash Equlibrium Profile Utility for Player 2: ", - NE_utility)
-            print("Nash Equlibrium Player 2 Strategy: ", filtered_player2_NE)
+            if verbose:
+                print("Player 2 strategy solving time: ", time.time() - start_time)
+                print("Nash Equlibrium Profile Utility for Player 2: ", - NE_utility)
+                print("Nash Equlibrium Player 2 Strategy: ", filtered_player2_NE)
+                print("------------------------------------------")
             return (filtered_player2_NE, NE_utility)
         else:
             print(LP_model.Status, GRB.Status.OPTIMAL)
