ee567d403753e36ec5cd3f95a9be978c5105fcaf
shuoyang2000 <yangs1@seas.upenn.edu>
2024-03-16 17:38:08 -0400

    add pyyaml

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(name='nash_equilibrium_solver',
       author_email='yangs1@seas.upenn.edu',
       package_dir={'': 'NE_solver'},
       install_requires=['gurobipy~=11.0.0',
-                        'numpy~=1.26.4']
+                        'numpy~=1.26.4',
+                        'pyyaml~=6.0.1']
       )
