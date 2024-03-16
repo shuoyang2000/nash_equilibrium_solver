6bee4509c43f99984277a5b5f484b7a1dbba915f
shuoyang2000 <yangs1@seas.upenn.edu>
2024-03-16 18:37:11 -0400

    update setup

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(name='nash_equilibrium_solver',
       version='0.0.0',
       author='Shuo Yang',
       author_email='yangs1@seas.upenn.edu',
-      package_dir={'': 'NE_solver'},
       install_requires=['gurobipy~=11.0.0',
                         'numpy~=1.26.4',
                         'pyyaml~=6.0.1']
